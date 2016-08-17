b86dae1d4e03b2819f071ebafc4af89bdcfd6b54
ollemesh <ollemesh@gmail.com>
2016-08-18 01:50:20 +0300

    bugs have fixed

--- save.js
+++
@@ -1,9 +0,0 @@
-function getSavedFriendsElements() {
-	if(localStorage.friendsList)
-		return JSON.parse(localStorage.friendsList).elements;
-	else return [];
-}
-
-function saveFriendsList() {
-	localStorage.friendsList = JSON.stringify(this);
-}
